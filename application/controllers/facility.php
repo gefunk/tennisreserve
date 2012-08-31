50915937354b23ec54eb20f9a6ebf3fc22a18c3a
Rahul Gokulnath <rahul.gokulnath@gmail.com>
2012-08-31 09:38:47 -0400

    Updates for time calendar
    Time calendar

--- application/controllers/facility.php
+++ application/controllers/facility.php
@@ -6,6 +6,7 @@ class Facility extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('facilities');
+
 	}
 	
 	function signup()
@@ -43,10 +44,58 @@ class Facility extends CI_Controller {
 	
 	function courts()
 	{
+		$prefs = array (
+		               'show_next_prev'  => TRUE,
+		               'next_prev_url'   => 'http://example.com/index.php/calendar/show/'
+		           	);
+	
+		$prefs['template'] = '
+
+		   {table_open}<table border="0" cellpadding="4" cellspacing="4">{/table_open}
+
+		   {heading_row_start}<tr>{/heading_row_start}
+
+		   {heading_previous_cell}<th><a href="{previous_url}">&lt;&lt;</a></th>{/heading_previous_cell}
+		   {heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
+		   {heading_next_cell}<th><a href="{next_url}">&gt;&gt;</a></th>{/heading_next_cell}
+
+		   {heading_row_end}</tr>{/heading_row_end}
+
+		   {week_row_start}<tr>{/week_row_start}
+		   {week_day_cell}<td>{week_day}</td>{/week_day_cell}
+		   {week_row_end}</tr>{/week_row_end}
+
+		   {cal_row_start}<tr>{/cal_row_start}
+		   {cal_cell_start}<td>{/cal_cell_start}
+
+		   {cal_cell_content}<a href="{content}">{day}</a>{/cal_cell_content}
+		   {cal_cell_content_today}<div class="highlight"><a href="{content}">{day}</a></div>{/cal_cell_content_today}
+
+		   {cal_cell_no_content}{day}{/cal_cell_no_content}
+		   {cal_cell_no_content_today}<div class="highlight">{day}</div>{/cal_cell_no_content_today}
+
+		   {cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+		   {cal_cell_end}</td>{/cal_cell_end}
+		   {cal_row_end}</tr>{/cal_row_end}
+
+		   {table_close}</table>{/table_close}
+		';
+		
+		
+		$this->load->library('calendar', $prefs);
+
 		$data['title'] = 'Court Management';
 		$this->load->view('templates/header', $data);
 		$this->load->view('facility/courts');
 		$this->load->view('templates/footer');
+		
+	}
+	
+	function test_calendar()
+	{
+		$this->load->library('calendar');
+		echo $this->calendar->generate();
 	}
 	
 }
--- application/views/facility/courts.php
+++ application/views/facility/courts.php
@@ -1,17 +1,90 @@
 <link href="<?php echo base_url();?>assets/css/court_management.css" rel="stylesheet">
 
 <script language="javascript">
+var clicked_row = null;
+var row_index = null;
+
 	$(document).ready(function(){
 		$("#courts > li").click(function(e){
 			$(this).addClass('active').siblings('li').removeClass('active');
-		})
+		});
+		
+		/*
+			When a row is clicked it adds a div to the clicked row
+			when the a second row is clicked it extends the div to that row
+		*/
+		$("table#calendar tr").click(function(e){
+			// check if there already been a row clicked
+			if(!clicked_row){
+				/* 
+					there hasn't been a table row clicked, 
+					set the clicked_row variable to equal the currently clicked row
+				*/
+				clicked_row = this;
+				// get the table cell, position
+				var tdcell = $(this).children('td:eq(1)');
+				var col_position = $(tdcell).position();
+				// get the table position so we can calcuate the relative position
+				var table_calendar_position = $("table#calendar").position();
+				// calculate the top location of where the iv should be added
+				var top = Math.abs(table_calendar_position.top-col_position.top)+3;
+				// add the div to the page on top of the table
+				$("#calendar-container > table").before('<div id="booking_example" class="booking" rel="popover" style="top:'+top+'px;left:'+(col_position.left+5)+'px;height:'+$(tdcell).css('height')+';width:'+($(tdcell).width()-10)+'px"><p>Rahul Gokulnath and Pritesh Parekh</p></div>');
+			}else{
+				// get the cell where the div should end on
+				var tdcell = $(this).children('td:eq(1)');
+				// calculate the position of the cell
+				var col_position = $(tdcell).position();
+
+				// get the div and the position of the div
+				var booking_div = $("#booking_example");
+				var booking_position = $(booking_div).position();
+				// set the div height to the end div
+				$(booking_div).height(Math.abs((col_position.top+$(tdcell).height()) - booking_position.top));
+				// clear the clicked_row variable
+				clicked_row = null;
+			}
+		}).hover(
+			function(e){
+				if(clicked_row){
+					$(this).children('td:eq(1)').css('background-color','LightSkyBlue');
+				}
+			},
+			function(e){
+				$(this).children('td:eq(1)').css('background-color','');
+			}
+		);
+		
+		$('#calendar-container').on('click','div.booking', function(){
+			var div_value = $(this).children('p').text();
+			var options = {
+				animation: true,
+				html: true,
+				trigger: 'click',
+				placement: 'right',
+				title: div_value,
+				content: div_value,
+				delay: { show: 500, hide: 100 }
+			}
+			$(this).popover(options);
+		});
+		
+
 		
 	});
 
 </script>
 
 <h2>Court Management</h2>
-
+<div class="popover right">
+	<div class="arrow"></div>
+	<h3 class="popover-title">Popover right</h3>
+	<div class="popover-content">
+		<p>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+		<button>Cancel</button>
+		<button>Save</button>
+	</div>
+</div>
 <div class="row">
 	<div class="span3">
 		<ul id="courts" class="nav nav-pills nav-stacked">
@@ -101,203 +174,206 @@
 			</div><!-- end court information -->
 			<div id="court-calendar" class="tab-pane active">
 				<div id="calendar-title">
-					<h2>Thursday, Aug 30</h2>
+					<h2>Thursday, Aug 30, 2012</h2>
 				</div>
 				<div id="calendar-container">
-					<table id="calendar" class="table table-bordered table-hover" cellspacing="5" cellpadding="5">
-						<tr>
+					<table id="calendar" class="table table-hover" cellspacing="5" cellpadding="5">
+						<tr id="00_00">
 							<td>12 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="00_30">
 							<td>12:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="01_00">
 							<td>1 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="01_30">
 							<td>1:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="02_00">
 							<td>2 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="02_30">
 							<td>2:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="03_00">
 							<td>3 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="03_30">
 							<td>3:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="04_00">
 							<td>4 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="04_30">
 							<td>4:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="05_00">
 							<td>5 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="05_30">
 							<td>5:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="06_00">
 							<td>6 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="06_30">
 							<td>6:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="07_00">
 							<td>7 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="07_30">
 							<td>7:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="08_00">
 							<td>8 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="08_30">
 							<td>8:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="09_00">
 							<td>9 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="09_30">
 							<td>9:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="10_00">
 							<td>10 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="10_30">
 							<td>10:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="11_00">
 							<td>11 AM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="11_30">
 							<td>11:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="12_00">
 							<td>12 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="12_30">
 							<td>12:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="13_00">
 							<td>1 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="13_30">
 							<td>1:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="14_00">
 							<td>2 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="14_30">
 							<td>2:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="15_00">
 							<td>3 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="15_30">
 							<td>3:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="16_00">
 							<td>4 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="16_30">
 							<td>4:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="17_00">
 							<td>5 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="17_30">
 							<td>5:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="18_00">
 							<td>6 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="18_30">
 							<td>6:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="19_00">
 							<td>7 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="19_30">
 							<td>7:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="20_00">
 							<td>8 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="20_30">
 							<td>8:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="21_00">
 							<td>9 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="21_30">
 							<td>9:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="22_00">
 							<td>10 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="22_30">
 							<td>10:30</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="23_00">
 							<td>11 PM</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr id="23_30">
 							<td>11:30</td>
 							<td></td>
 						</tr>
 					</table>
+				</div>
+				<div id="month-calendar">
+					<?php echo $this->calendar->generate($this->uri->segment(3), $this->uri->segment(4)); ?>
 				</div>
 				
 			</div><!-- end court calendar tab -->
