57c5b462376361535320b784613c95a8f1522af7
Rahul Gokulnath <rahul.gokulnath@gmail.com>
2012-10-12 13:45:17 -0400

    Changed Navigation Title
    changed navigation title to new header

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -3,36 +3,45 @@
 <div class="home hero-unit">
 	<h1>Manage your Courts</h1>  
 	<small>easily ,efficiently, elegantly</small>
+	<div>
+		<a href="<?php echo site_url('facility/signup'); ?>" class='btn btn-primary btn-large'>Sign Up for free!</a>
+	</div>
 </div>
 
 <!-- Example row of columns -->
 <div class="row">
   <div class="span6">
+	<i class="icon-calendar icon-large"></i>
     <h4>View Reservations for all Courts</h4>
     <p>Remove pages of calendar clutter from your desk. Keep all reservations safe and secure - accessible online, anytime, anywhere</p>
     
   </div>
   <div class="span6">
+	<i class="icon-money icon-large"></i>
     <h4>Improve Efficiency</h4>
     <p>Minimize time spent on the phone, Allow users to view court calendar and requests reservations. Save time and money by being more organized</p>
  </div>
 </div>
 <div class="row">
 	<div class="span6">
-    	<h4>Reduce Errors</h4>
-	    <p>Remove any doubt that a court has been booked. A user can log on online and check their reservation immediately</p>
+		<i class="icon-pencil icon-large"></i>
+		<h4>Reduce Errors</h4>
+		<p>Remove any doubt that a court has been booked. A user can log on online and check their reservation immediately</p>
   	</div>	
 	<div class="span6">
+		<i class="icon-magic icon-large"></i>
     	<h4>No Infrastructure</h4>
 	    <p>We host everything! Our servers are reliable, redundant and 100% secure. You don't have to own any hardware!</p>
   	</div>
 </div>
 <div class="row">
 	<div class="span6">
+		<i class="icon-cloud icon-large"></i>
     	<h4>Get your own Domain</h4>
 	    <p>Users can access your courts at yourdomain.savecourt.com, When you sign up you will have your own space at savecourt.com</p>
   	</div>	
 	<div class="span6">
+		<i class="icon-tags icon-large"></i>
     	<h4>Advertise with us!</h4>
 	    <p>Get more people to buy merchandise at your pro shop or stores. Also advertise your services on our site!</p>
   	</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,13 +11,8 @@
 	<script src="<?php echo base_url();?>assets/js/global.js"></script>
     <!-- Le styles -->
     <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-awesome.css">
     <link href="<?php echo base_url();?>assets/css/jquery-ui/jquery-ui.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-    </style>
     <link href="<?php echo base_url();?>assets/css/bootstrap-responsive.css" rel="stylesheet">
 	<link href="<?php echo base_url();?>assets/css/global.css" rel="stylesheet">
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -34,50 +29,39 @@
   </head>
 
   <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="<?php echo site_url('home');?>">Save Court</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="<?php echo site_url('home');?>">Home</a></li>
-              <li><a href="<?php echo site_url('about');?>">About</a></li>
-              <li><a href="<?php echo site_url('contact');?>">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="nav-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-			  <?php if($title == 'Calendar') { ?>
-			  <li class="dropdown active">
+	<div id="title">
+		<div id="text" class="depth">
+			Save Court
+		</div>
+	</div>
+    <div id="nav">
+        <ul>
+          <li><a class="selected" href="<?php echo site_url('home');?>">HOME</a></li>
+          <li><a href="<?php echo site_url('about');?>">ABOUT</a></li>
+          <li><a href="<?php echo site_url('contact');?>">CONTACT</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li class="nav-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+ 		<?php if($title == 'Calendar') { ?>
+ 			<li class="dropdown active">
 				<a id="date" href="#" class="dropdown-toggle" data-toggle="dropdown">Calendar <b class="caret"></b></a>
 				<ul class="dropdown-menu">
-                  <li id="calendar" ><?php echo $this->calendar->generate(); ?></li>
-                </ul>
-              </li>
-			  <?php } ?>
+              <li id="calendar" ><?php echo $this->calendar->generate(); ?></li>
             </ul>
-            <form class="navbar-form pull-right">
-              <input class="span2" type="text" placeholder="Email">
-              <input class="span2" type="password" placeholder="Password">
-              <button type="submit" class="btn">Sign in</button>
-            </form>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
+          </li>
+ <?php } ?>
+        </ul>
+
+        <button type="submit" class="btn">Sign in</button>
     </div>
 
     <div class="container">
